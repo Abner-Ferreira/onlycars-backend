0f14b9b010ba7fbcb46dfe57c2247b50747c4fb0
Abner-Ferreira <rm93576@fiap.com.br>
2023-11-12 14:16:17 -0300

    index file modified

--- index.js
+++ index.js
@@ -11,105 +11,100 @@ app.use(cors({ origin: "*" }))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-const pool = mysql.createPool({
-    connectionLimit: 10,
-    host: "localhost",
-    user: "root",
-    password: "123456",
-    database: "carros",
-    debug: false
-});
-
-// const db = mysql.createConnection({
+// const pool = mysql.createPool({
+//     connectionLimit: 10,
 //     host: "localhost",
 //     user: "root",
 //     password: "123456",
-//     database: "carros"
-// })
+//     database: "carros",
+//     debug: false
+// });
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "123456",
+    database: "carros"
+})
 
 
-pool.getConnection((err, connection) => {
-    if (err) throw err;
-    app.get("/carros", (req, res) => {
-        const q = "SELECT * FROM carro"
-        connection.query(q, (err, data) => {
-            if (err) return res.send(err)
-            return res.json(data)
-        })
-    })
-
-    app.get("/carros/:id", (req, res) => {
-
-        const carId = req.params.id
-
-        const q = "SELECT * FROM carro WHERE idCarro = ?"
-
-        connection.query(q, [carId], (err, data) => {
-            if (err) return res.send(err)
-            return res.json(data)
-        })
-    })
-
-
-
-
-
-    app.post("/carros", (req, res) => {
-        const q = "INSERT INTO carro (`nmCarro`, `marcaCarro`, `descCarro`, `precoCarro`, `anoCarro`, `imgCarro`) VALUES (?)"
-
-
-        const carros = [
-            req.body.nmCarro,
-            req.body.marcaCarro,
-            req.body.descCarro,
-            req.body.precoCarro,
-            req.body.anoCarro,
-            req.body.imgCarro,
-        ]
-
-
-
-        connection.query(q, [carros], (err, data) => {
-            if (err) return res.send(err)
-            return res.json("Car has been insert successfully")
-        })
-    })
-
-
-    app.put("/carros/:id", (req, res) => {
-        const carId = req.params.id
-
-        const q = "UPDATE  carro SET `nmCarro` = ?, `marcaCarro` = ?, `descCarro` = ?, `precoCarro` = ?, `anoCarro`= ? , `imgCarro` = ? WHERE idCarro = ?"
-
-        const carModified = [
-            req.body.nmCarro,
-            req.body.marcaCarro,
-            req.body.descCarro,
-            req.body.precoCarro,
-            req.body.anoCarro,
-            req.body.imgCarro
-        ]
-
-        connection.query(q, [...carModified, carId], (err, data) => {
-            if (err) return res.send(err)
-            return res.json("Car has been update successfully")
-        })
-    })
-
-    app.delete("/carros/:id", (req, res) => {
-        const carId = req.params.id
-
-        const q = "DELETE FROM carro WHERE idCarro = ?"
-
-        connection.query(q, [carId], (err, data) => {
-            if (err) return res.send(err)
-            return res.json("Car has been delete successfully")
-        })
-
+app.get("/carros", (req, res) => {
+    const q = "SELECT * FROM carro"
+    db.query(q, (err, data) => {
+        if (err) return res.send(err)
+        return res.json(data)
     })
 })
 
-// 
+app.get("/carros/:id", (req, res) => {
+
+    const carId = req.params.id
+
+    const q = "SELECT * FROM carro WHERE idCarro = ?"
+
+    db.query(q, [carId], (err, data) => {
+        if (err) return res.send(err)
+        return res.json(data)
+    })
+})
+
+
+
+
+
+app.post("/carros", (req, res) => {
+    const q = "INSERT INTO carro (`nmCarro`, `marcaCarro`, `descCarro`, `precoCarro`, `anoCarro`, `imgCarro`) VALUES (?)"
+
+
+    const carros = [
+        req.body.nmCarro,
+        req.body.marcaCarro,
+        req.body.descCarro,
+        req.body.precoCarro,
+        req.body.anoCarro,
+        req.body.imgCarro,
+    ]
+
+
+
+    db.query(q, [carros], (err, data) => {
+        if (err) return res.send(err)
+        return res.json("Car has been insert successfully")
+    })
+})
+
+
+app.put("/carros/:id", (req, res) => {
+    const carId = req.params.id
+
+    const q = "UPDATE  carro SET `nmCarro` = ?, `marcaCarro` = ?, `descCarro` = ?, `precoCarro` = ?, `anoCarro`= ? , `imgCarro` = ? WHERE idCarro = ?"
+
+    const carModified = [
+        req.body.nmCarro,
+        req.body.marcaCarro,
+        req.body.descCarro,
+        req.body.precoCarro,
+        req.body.anoCarro,
+        req.body.imgCarro
+    ]
+
+    db.query(q, [...carModified, carId], (err, data) => {
+        if (err) return res.send(err)
+        return res.json("Car has been update successfully")
+    })
+})
+
+app.delete("/carros/:id", (req, res) => {
+    const carId = req.params.id
+
+    const q = "DELETE FROM carro WHERE idCarro = ?"
+
+    db.query(q, [carId], (err, data) => {
+        if (err) return res.send(err)
+        return res.json("Car has been delete successfully")
+    })
+
+})
 
 
 
